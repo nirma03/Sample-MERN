c0aafd3f765b72359d51bc2a680db505493d737f
nirma03 <nirmasarangi@gmail.com>
2021-02-10 01:07:39 +0530

    register done

--- Frontend/frontend/src/Components/Register.js
+++ Frontend/frontend/src/Components/Register.js
@@ -12,8 +12,116 @@ import {
     MDBInput
 } from 'mdbreact';
 import "../css/Login.css";
+import axios from "axios";
+import {toast} from "react-toastify";
+import {serverUrl} from "./config";
+import {Form} from "react-bootstrap";
+import "react-toastify/dist/ReactToastify.css";
 
+toast.configure();
 class Register extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeConfirmPassword = this.onChangeConfirmPassword.bind(this);
+        this.onChangeContact= this.onChangeContact.bind(this);
+
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+            contact: ""
+        };
+    }
+
+    // updateInput(key, value) {
+    //     this.setState({
+    //         [key]: value,
+    //     });
+    // }
+    onChangeUsername(event){
+        this.setState({
+            username: event.target.value,
+            [event.target.name]: event.target.value
+
+        })
+    }
+    onChangeEmail(event){
+        this.setState({
+            email: event.target.value,
+            [event.target.name]: event.target.value
+
+        })
+    }
+    onChangePassword(event){
+        this.setState({
+            password: event.target.value,
+            [event.target.name]: event.target.value
+
+        })
+    }
+    onChangeConfirmPassword(event){
+        this.setState({
+            confirmPassword: event.target.value,
+            [event.target.name]: event.target.value
+
+        })
+    }
+    onChangeContact(event){
+        this.setState({
+            contact: event.target.value,
+            [event.target.name]: event.target.value
+
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        console.log("Submit button");
+        const users = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password,
+            contact: this.state.contact,
+        };
+
+        if (this.state.password === this.state.confirmPassword) {
+            axios
+                .post(serverUrl + "/users/add", users)
+                .then((response) => {
+                    console.log(users);
+                    toast("Register Successful! \n You will be redirected to Login Page");
+                    console.log("User added!")
+                    setTimeout(() => {
+                        window.location = "/login";
+                    }, 5000);
+                })
+                .catch((error) => {
+                    console.log(error.response);
+                    toast("Email or Username Exists");
+                    this.setState({
+                        username: "",
+                        email: "",
+                        password: "",
+                        confirmPassword: "",
+                        contact:""
+                    });
+                });
+        } else {
+            toast("Password doesn't match");
+            this.setState({
+                password: "",
+                confirmPassword: "",
+            });
+        }
+    }
+
     state = {
         collapseID: ''
     };
@@ -65,19 +173,29 @@ class Register extends Component {
                             <MDBCol md='6' xl='5' className='mb-4'>
                                 <MDBCard className='dark-grey-text'>
                                     <MDBCardBody className='z-depth-2'>
+                                        <Form onSubmit={this.onSubmit} c>
                                         <h3 className='dark-grey-text text-center'>
                                             <strong>Sign Up</strong>
                                         </h3>
                                         <hr />
-                                        <MDBInput label='Email' icon='user' type="email" />
-                                        <MDBInput label='Password' icon='envelope' />
-                                        <MDBInput label='Email' icon='user' type="email" />
-                                        <MDBInput label='Password' icon='envelope' />
-                                        <MDBInput label='Email' icon='user' type="email" />
-                                        <MDBInput label='Password' icon='envelope' />
+                                        <MDBInput label='Username' icon='user' type="text" onChange={this.onChangeUsername}
+                                                  value={this.state.username} name="username"
+                                                  required />
+                                        <MDBInput label='Email' icon='envelope' type="email" onChange={this.onChangeEmail}
+                                                  value={this.state.email} name="email"
+                                                  required/>
+                                        <MDBInput label='Password' icon='user' type="password" onChange={this.onChangePassword}
+                                                  value={this.state.password} name="password"
+                                                  required />
+                                        <MDBInput label='Confirm Password' icon='envelope' type="password" onChange={this.onChangeConfirmPassword}
+                                                  value={this.state.confirmPassword} name="confirmPassword"
+                                                  required />
+                                        <MDBInput label='Contact' icon='user' type="text" onChange={this.onChangeContact}
+                                                  value={this.state.contact} name="contact"
+                                                  required />
                                         <div className='text-center mt-3 black-text'>
-                                            <MDBBtn color='indigo'>Send</MDBBtn>
-                                            <MDBBtn color='indigo'>Send</MDBBtn>
+                                            <MDBBtn color='indigo' type="submit" >Sign Up</MDBBtn>
+                                            <MDBBtn color='indigo'>Clear</MDBBtn>
                                             <br/>
                                             <br/>
                                             <MDBModalFooter>
@@ -94,6 +212,7 @@ class Register extends Component {
                                                 </div>
                                             </MDBModalFooter>
                                         </div>
+                                        </Form>
                                     </MDBCardBody>
                                 </MDBCard>
                             </MDBCol>
